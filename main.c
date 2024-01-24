3a5311cc089185751d133a3db0da2c564b9311f0
theoliss <theo.lissarrague@gmail.com>
2024-01-24 10:52:16 +0100

    add cut_block

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 #define MIN_BLOCK_LENGTH 1024
+#define HEADER_LENGTH sizeof(block_header)
 
 typedef struct block_header
 {
-    int length;
+    size_t length;
     enum { occupied = 1, free = 0 } flag;
     struct block_header *next ;
 } block_header;
@@ -30,9 +31,21 @@ int insert_block()
 
 }
 
-void cut_block(block_header target, int size)
+block_header* cut_block(block_header *target, int size)
 {
+    if (target->flag == free && target->length > size + HEADER_LENGTH)
+    {
+        block_header *new_block = target + HEADER_LENGTH +size;
+        new_block->next = target->next;
+        new_block->flag = free;
+        new_block->length = target->length - size - HEADER_LENGTH;
 
+        target->length = size;
+        target->next = new_block;
+
+        return new_block;
+    }
+    return NULL;
 }
 
 int main()
