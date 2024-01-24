9ca8a5c64d668690845d7cdb9637fc2f26f4ef2c
xRayz3n <sebastien_lam@hotmail.fr>
2024-01-24 10:51:28 +0100

    Added create_block function & few fixes

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
-
+#include <stdio.h>
 #define MIN_BLOCK_LENGTH 1024
+#define HEADER_LENGTH sizeof(block_header)
 
 typedef struct block_header
 {
-    int length;
-    enum flag { occupied = 1, free = 0 };
-    struct block_header *previous ;
-    struct block_header *next ;
+    size_t length;
+    enum flag { OCCUPIED = 1, FREE = 0 } flag;
+    struct block_header* next ;
 } block_header;
 
-void* mozalloc(int size)
+void* mozalloc(size_t size)
 {
 
 }
@@ -20,9 +20,20 @@ void mozafree(void* target)
 
 }
 
-block_header create_block() // function I need to write
+block_header* create_block(size_t size) // function I need to write
 {
-    
+    block_header* extension;
+    if (size < MIN_BLOCK_LENGTH) size = MIN_BLOCK_LENGTH;
+
+    extension = sbrk(size + HEADER_LENGTH);
+    if (extension == (void *) -1) return NULL;
+
+    extension->length = size;
+    extension->flag = FREE;
+    extension->next = extension;
+
+    return extension;
+
 }
 
 int insert_block()
@@ -37,5 +48,16 @@ void cut_block(struct block_header target, int size)
 
 int main()
 {
+block_header* block = create_block(2048);  // Allocate a block of size 2048 bytes
 
+    if (block != NULL) {
+        printf("Block created successfully:\n");
+        printf("Length: %zu\n", block->length);
+        printf("Flag: %s\n", (block->flag == FREE) ? "FREE" : "OCCUPIED");
+        printf("Next: %p\n", (void*)block->next);
+    } else {
+        printf("Failed to create block\n");
+    }
+
+    return 0;
 }
